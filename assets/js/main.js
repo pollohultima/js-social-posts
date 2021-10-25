2004fb6b7ca3b6011b1c38082b3ef2e1196db0dd
pollohultima <matteo.polonio@gmail.com>
2021-10-25 13:43:41 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+/* Creare una pagina che elenchi una lists di post socials a partire da un array di oggetti.
+Gli attributi minimi del modello di un post: id, contenuto, immagine,
+autore (nome, avatar), numero di likes, data creazione.
+Un secondo array conterrà solo gli id dei posts a cui abbiamo dato like.
+ */
