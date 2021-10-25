e484ab08cd99261ff5d822d77ba790b2a041a264
pollohultima <matteo.polonio@gmail.com>
2021-10-25 15:41:12 +0200

    Completing the 'for' for insert the images

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@ const posts = [
         id: 1,
         content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis libero magni fugiat quasi, neque animi dolorem maiores ratione, officia pariatur enim similique commodi eaque dolore rem quibusdam voluptas? Dolor, ipsa voluptate. Voluptas ducimus blanditiis quisquam voluptates asperiores, tenetur commodi, eveniet magnam consequuntur aspernatur quas doloremque ex odio. Libero, ut.',
         image: 'https://unsplash.it/300/300?image=',
+        avatar: 'https://unsplash.it/300/300?image=',
         user: 'Gigi Caglio',
         likes: 0,
         date: 0
@@ -18,6 +19,7 @@ const posts = [
         id: 2,
         content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis libero magni fugiat quasi, neque animi dolorem maiores ratione, officia pariatur enim similique commodi eaque dolore rem quibusdam voluptas? Dolor, ipsa voluptate. Voluptas ducimus blanditiis quisquam voluptates asperiores, tenetur commodi, eveniet magnam consequuntur aspernatur quas doloremque ex odio. Libero, ut.',
         image: 'https://unsplash.it/300/300?image=',
+        avatar: 'https://unsplash.it/300/300?image=',
         user: 'Mario Sucro',
         likes: 0,
         date: 0
@@ -27,6 +29,7 @@ const posts = [
         id: 3,
         content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis libero magni fugiat quasi, neque animi dolorem maiores ratione, officia pariatur enim similique commodi eaque dolore rem quibusdam voluptas? Dolor, ipsa voluptate. Voluptas ducimus blanditiis quisquam voluptates asperiores, tenetur commodi, eveniet magnam consequuntur aspernatur quas doloremque ex odio. Libero, ut.',
         image: 'https://unsplash.it/300/300?image=',
+        avatar: 'https://unsplash.it/300/300?image=',
         user: 'Angelo Pappino',
         likes: 0,
         date: 0
@@ -35,8 +38,38 @@ const posts = [
         id: 4,
         content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis libero magni fugiat quasi, neque animi dolorem maiores ratione, officia pariatur enim similique commodi eaque dolore rem quibusdam voluptas? Dolor, ipsa voluptate. Voluptas ducimus blanditiis quisquam voluptates asperiores, tenetur commodi, eveniet magnam consequuntur aspernatur quas doloremque ex odio. Libero, ut.',
         image: 'https://unsplash.it/300/300?image=',
+        avatar: 'https://unsplash.it/300/300?image=',
         user: 'Diego Culino',
         likes: 0,
         date: 0
     },
 ]
+
+const liked = []
+
+for (let i = 0; i < posts.length; i++) {
+    const post = posts[i];
+
+    const element = `
+    <div class="col-7">
+                        <div class="card">
+                            <div class="heading">
+                                <img class="avatar" src=${post.avatar} alt="">
+                                <h3 class="user">${post.user}</h3>
+                                <h6 class="date">${post.date}</h6>
+                            </div>
+                            <div class="hero_image">
+                                <img class="post_image" src=${post.image} alt="">
+                                <p class="caption">${post.content}</p>
+                            </div>
+                            <div class="buttons">
+                                <button class="like"></button>
+                                <div class="like_counter">${post.likes}</div>
+                            </div>
+                        </div>
+                    </div>
+    
+    `
+    document.query
+}
+
